09e13cb76d762cc5572c205fcbf15ac468069521
Marti-SV <a21marsalval@inspedralbes.cat>
2023-01-31 12:43:12 +0100

    Fix default validation error

--- front/src/pages/Game.js
+++ front/src/pages/Game.js
@@ -1,9 +1,11 @@
 import "../normalize.css";
+import Logout from "../components/LogoutButton"
 
 function Game() {
   return (
     <div>
       <h1>Game</h1>
+      <Logout></Logout>
     </div>
   );
 }
--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -2,27 +2,33 @@ import "../normalize.css";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import routeFetch from "../index"
+import session from "../components/UserSession";
+
 
 function Login() {
-    const [login, setLogin] = useState(false);
+    const [login, setLogin] = useState(0);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [mantenerSesion, setMantenerSesion] = useState(false);
 
     useEffect(() => {
-        const user = new FormData()
-        user.append("email", email);
-        user.append("password", password);
+        if (login != 0) {
+            const user = new FormData()
+            user.append("email", email);
+            user.append("password", password);
 
-        fetch(routeFetch + "/api/login", {
-            method: 'POST',
-            body: user
-        })
-            .then((response) => response.json())
-            .then((data) =>
-                console.log(data)
-            );
-
+            fetch(routeFetch + "/api/login", {
+                method: 'POST',
+                body: user
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    session.setData(data);
+                    console.log(session.getData());
+                }
+                );
+        }
+        
     }, [login]);
     return (
         <div>
@@ -34,7 +40,7 @@ function Login() {
             <Link to="/">
                 <button>Go back</button>
             </Link>
-            <button onClick={() => setLogin(!login)}>Login</button>
+            <button onClick={() => setLogin(login + 1)}>Login</button>
             <Link to="/forgotPassword">
                 I forgot my password
             </Link>
