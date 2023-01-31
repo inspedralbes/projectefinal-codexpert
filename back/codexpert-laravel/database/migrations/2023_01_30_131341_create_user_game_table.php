821eedd95c5fef9c0e5e990194a829b95a7b622a
Marti-SV <a21marsalval@inspedralbes.cat>
2023-01-31 08:33:59 +0100

    Fix migrations error and password hashed

--- back/codexpert-laravel/database/migrations/2023_01_30_131341_create_user_game_table.php
+++ back/codexpert-laravel/database/migrations/2023_01_30_131341_create_user_game_table.php
@@ -16,19 +16,15 @@ return new class extends Migration
         Schema::create('user_game', function (Blueprint $table) {
             $table->id();
 
-
             $table->bigInteger('game_id')->unsigned()->index();
             $table->bigInteger('user_id')->unsigned()->index();
-
 
             $table->integer('hearts_remaining')->default(3);
             $table->integer('perks_used')->default(0);
             $table->integer('question_at')->default(5);
 
-
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('game_id')->references('id')->on('game')->onDelete('cascade');
-
 
             $table->timestamps();
         });
