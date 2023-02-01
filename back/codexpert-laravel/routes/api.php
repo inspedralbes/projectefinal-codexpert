e6fb526f39edb1c1b344e6700a9d8fc4c70f8278
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2023-02-01 12:11:25 +0100

    Fixed issue, imported user controller on api.php

--- back/codexpert-laravel/routes/api.php
+++ back/codexpert-laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,6 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('/getAvatar', [AuthController::class, 'getAvatar']);
+Route::get('/getAvatar', [UserController::class, 'getAvatar']);
 
-Route::post('/setAvatar', [AuthController::class, 'setAvatar']);
+Route::post('/setAvatar', [UserController::class, 'setAvatar']);
