a4e593500bfc5213b81f3472d4c39b666eea8ad9
Marti-SV <a21marsalval@inspedralbes.cat>
2023-02-02 12:08:23 +0100

    fetch to web without cors on frontend

--- back/codexpert-laravel/routes/web.php
+++ back/codexpert-laravel/routes/web.php
@@ -1,6 +1,10 @@
 <?php
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
