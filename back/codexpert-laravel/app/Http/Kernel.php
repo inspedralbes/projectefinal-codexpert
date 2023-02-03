22ec6fa41d2fa165b8704ad8daa10fc0f07da258
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2023-02-03 08:47:22 +0100

    Removed session in kernel

--- back/codexpert-laravel/app/Http/Kernel.php
+++ back/codexpert-laravel/app/Http/Kernel.php
@@ -34,7 +34,6 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -46,7 +45,6 @@ class Kernel extends HttpKernel
             //PPF 'throttle:api',
            //PPF \Illuminate\Routing\Middleware\SubstituteBindings::class,
            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\Session\Middleware\AuthenticateSession::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
