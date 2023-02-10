e18997ec380ce4112341b614af8a8f9f4b3dc4d2
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2023-02-10 13:00:39 +0100

    Add checkAnswer

--- back/codexpert-laravel/app/Http/Controllers/GameController.php
+++ back/codexpert-laravel/app/Http/Controllers/GameController.php
@@ -48,8 +48,12 @@ class GameController extends Controller
             'question' => (object) [
                 'idQuestion' => $getQuestions -> id,
                 'statement' => $getQuestions -> statement,
-                'inputs' => [$getQuestions -> userExpectedInput, $getQuestions -> testInput1, $getQuestions -> testInput2],
-                'outputs' => [$getQuestions -> userExpectedOutput, $getQuestions -> testOutput1, $getQuestions -> testOutput2]
+                'input' => $getQuestions -> userExpectedInput,
+                'expectedOutput' => $getQuestions -> userExpectedOutput, 
+                'testInput1' => $getQuestions -> testInput1, 
+                'testOutput1' => $getQuestions -> testOutput1, 
+                'testInput2' => $getQuestions -> testInput2, 
+                'testOutput2' => $getQuestions -> testOutput2                    
             ]
             ];
 
@@ -59,15 +63,17 @@ class GameController extends Controller
     public function checkAnswer(Request $request)
     {
         $answerValidation = [];
-        
+
         $request -> idQuestion;
         $request -> idGame;
         $request -> idUser;
         $request -> evalRes;
         
         $question = Question::find($request -> idQuestion) -> first();
-        $question -> userExpectedOutput = serialize(array( 3, 5, 7 ));
-
+        $question -> userExpectedOutput;
+        $question -> testInput1;
+        $question -> testInput2;
+        
         return response() -> json($answerValidation);
     }    
     
