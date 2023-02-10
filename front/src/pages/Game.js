1b755b662d83a72bb02727bf3dadc444d851a0bb
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-02-10 10:00:27 +0100

    recieve questions in game component

--- front/src/pages/Game.js
+++ front/src/pages/Game.js
@@ -8,6 +8,11 @@ import Chat from "../components/Chat";
 function Game({ socket }) {
   const [messages, setMessages] = useState([]);
   const [msg, setMsg] = useState("");
+  const [qst, setQst] = useState({
+    // statement: "",
+    // input: "",
+    // expectedOutput: "",
+  });
 
   const handleSendMessage = (e) => {
     e.preventDefault();
@@ -24,36 +29,72 @@ function Game({ socket }) {
     });
   }, []);
 
+  useEffect(() => {
+    socket.on("game_data", function (data) {
+      console.log(data);
+      setQst(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(qst);
+  }, [qst]);
+
   return (
     <div className="game">
       <div className="game__statement">
-        <h1 className="game__statementTitle">statement</h1>
+        <h1 className="game__statementTitle">{qst.statement}</h1>
       </div>
       <div className="game--grid">
         <div className="game__expectedInput">
-          <h1>input</h1>
+          <h1>{qst.input.toString()}</h1>
         </div>
         <div className="game__expectedOutput">
-          <h1>expectedOutput</h1>
+          <h1>{qst.expectedOutput.toString()}</h1>
         </div>
       </div>
-      <div class="editor">
-
-        <div class="input-header">
+      <div className="editor">
+        <div className="input-header">
           <h1>Input</h1>
         </div>
-        <div class="file-window js-view">
-          <div class="line-numbers">
-            1<br />2<br />3<br />4<br />5<br />6<br />7<br />8<br />9<br />10<br />11<br />12<br />13<br />14<br />15<br />16<br />17<br />18<br />19<br />20
-          </div>
-          <textarea class="input-strobe" type="text" placeholder="Type in your code :)"></textarea>
-          <div>
-          </div>
-          <div class="help">
+        <div className="file-window js-view">
+          <div className="line-numbers">
+            1<br />2<br />3<br />4<br />5<br />6<br />7<br />8<br />9<br />
+            10
             <br />
-                /* <br />
-            This is your code input.<br />
-            You can, we trust you!! <br />*/
+            11
+            <br />
+            12
+            <br />
+            13
+            <br />
+            14
+            <br />
+            15
+            <br />
+            16
+            <br />
+            17
+            <br />
+            18
+            <br />
+            19
+            <br />
+            20
+          </div>
+          <textarea
+            className="input-strobe"
+            type="text"
+            placeholder="Type in your code :)"
+          ></textarea>
+          <div></div>
+          <div className="help">
+            <br />
+            /* <br />
+            This is your code input.
+            <br />
+            You can, we trust you!! <br />
+            */
           </div>
         </div>
       </div>
