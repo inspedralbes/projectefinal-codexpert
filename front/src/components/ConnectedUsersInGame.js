506a46c6808286cbd17214badde9071ee226bb81
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-02-15 23:22:37 +0100

    userlist styles

--- front/src/components/ConnectedUsersInGame.js
+++ front/src/components/ConnectedUsersInGame.js
@@ -17,44 +17,44 @@ function ConnectedUsersInGame({ socket }) {
             <div id="userList" className="connectedUsers__userList userList">
                 {userList.map((user, index) => {
                     return (
-                        <div className="userList__item item" key={index}>
-                            <div>{user.name}</div>
+                        <div className="userList__user user" key={index}>
+                            <div className="user__name">{user.name}</div>
                             <div> {user.hearts_remaining > 0 &&
                                 <img
                                     src={user.avatar}
                                     width="50px"
-                                    className="item__image"
+                                    className="user__image"
                                     alt={user.name + "'s avatar"}
                                 ></img>}
 
                                 {user.hearts_remaining == 0 &&
                                     <img src={require('../img/hearts/skull.gif')}
                                         width="50px"
-                                        className="item__image"
-                                        alt={"user lost"} />}
+                                        className="user__skull"
+                                        alt={user.name + " lost"} />}
                             </div>
 
                             <div>
                                 {user.hearts_remaining == 3 &&
                                     <img src={require('../img/hearts/three_hearts.png')}
                                         height="50px"
-                                        className="item__image"
+                                        className="user__health"
                                         alt={user.hearts_remaining + " hearts remaining"} />}
 
                                 {user.hearts_remaining == 2 &&
                                     <img src={require('../img/hearts/two_hearts.gif')}
                                         height="50px"
-                                        className="item__image"
+                                        className="user__health"
                                         alt={user.hearts_remaining + " hearts remaining"} />}
 
                                 {user.hearts_remaining == 1 &&
                                     <img src={require('../img/hearts/one_heart.gif')}
                                         height="50px"
-                                        className="item__image"
+                                        className="user__health"
                                         alt={user.hearts_remaining + " hearts remaining"} />}
                             </div>
 
-                            <div>
+                            <div className="user__level">
                                 Level: {user.question_at + 1}
                             </div>
 
