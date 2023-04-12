dd69369cfb6040784ef86fab29661e7daddc0ae0
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2023-04-12 13:12:56 +0200

    Added function to change the username

--- back/codexpert-laravel/app/Http/Controllers/UserController.php
+++ back/codexpert-laravel/app/Http/Controllers/UserController.php
@@ -55,14 +55,56 @@ class UserController extends Controller
         return response() -> json($returnUser);
     }
 
+    public function checkValidName($request, $userFound)
+    {
+        $nameRepeated = false;
+        $validName = (object) ['willChange' => true];
+        $validName = (object) ['willChange' => false,
+                               'error' => "Name already in use."];      
+                              
+                              
+        //Check if user has changed the name                      
+        if ($request -> session()->get('userId') != null) {
+            if ($userFound -> name == $request -> newName) {
+                $validName = (object) ['changed' => false, 'error' => "Name has not been modified, no changes were made."];  
+            } else {
+                //If the name has been modified, we check that is valid.
+                if (($request -> newName < 3) || ($request -> newName > 20)) {
+                    $validName = (object) ['willChange' => false, 'error' => "Name must have a minimum amount of 3 characters and 20 max."];
+                } else {
+                    //If the name is valid we check if it's not repeated.
+                    $getAllNames = User::get('name');
+                    for ($i = 0; $i < count($getAllNames); $i++) { 
+                        if ($request -> newName == $getAllNames[$i]) {
+                            $nameRepeated = true;
+                        }
+                    }
+                    if ($nameRepeated) {
+                        $validName = (object) ['willChange' => false,
+                        'error' => "Name already in use."]; 
+                    } else {
+                        $validName = (object) ['willChange' => true];
+                    }
+                }
+            }
+            
+        } 
+        return $validName;
+    }
+    
     public function setUsername(Request $request)
     {
+        $validName = (object) ['willChange' => true];
         if ($request -> session()->get('userId') != null) {
             $userFound = User::where('id', $request->session()->get('userId'))->first();
-            $returnUser = (object) ['name' => $userFound -> name,
-                                    'email' => $userFound -> avatar,
-                                    'avatar' => $userFound -> avatar,
-                                    ];
+            $validName = $this->checkValidName($request, $userFound);
+            if ($validName -> willChange) {
+                $userFound -> name = $request -> newName;
+                $userFound -> save(); 
+                $returnUser = (object) ['success' => "Name has been changed."];
+            } else {
+                $returnUser = (object) ['error' => $validName -> error];
+            }
         } else {
             $returnUser = (object) ['error' => "User is not logged in."];
         }
