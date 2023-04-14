80888b60a955daac51a3867176926e9c720c3795
alessiacrisafo <a21alecrinor@inspedralbes.cat>
2023-04-14 09:01:10 +0200

    Added comments to UserController and fixed the validation when changing the name

--- back/codexpert-laravel/app/Http/Controllers/UserController.php
+++ back/codexpert-laravel/app/Http/Controllers/UserController.php
@@ -10,6 +10,7 @@ class UserController extends Controller
 {
     public function getAvatar(Request $request)
     {
+        //Check if the user id is null, if not not we get the user's avatar.
         if ($request -> session()->get('userId') != null) {
             $userFound = User::where('id', $request->session()->get('userId'))->first();
             $returnAvatar = (object) ['url' => $userFound -> avatar];
@@ -20,8 +21,10 @@ class UserController extends Controller
         return response() -> json($returnAvatar);
     }
     
+
     public function setAvatar(Request $request)
     {
+        //Check if the user is id null, if not we change the avatar.
         if ($request -> session()->get('userId') != null) {
             $userFound = User::where('id', $request->session()->get('userId'))->first();
             $userFound -> avatar = $request -> newAvatar;
@@ -33,15 +36,19 @@ class UserController extends Controller
         return response() -> json($returnResponse);
     }
 
+
     public function getRanking(Request $request)
     {
+        //Return the list of users ordered by their elo.
         $ranking = User::orderBy('elo', 'DESC')->get();
 
         return response() -> json($ranking);
     }    
     
+
     public function getUserData(Request $request)
     {
+        //If the user id is not null we return the information from the user (name, email, avatar)
         if ($request -> session()->get('userId') != null) {
             $userFound = User::where('id', $request->session()->get('userId'))->first();
             $returnUser = (object) ['name' => $userFound -> name,
@@ -55,21 +62,19 @@ class UserController extends Controller
         return response() -> json($returnUser);
     }
 
+
     public function checkValidName($request, $userFound)
     {
         $nameRepeated = false;
         $validName = (object) ['willChange' => true];
-        $validName = (object) ['willChange' => false,
-                               'error' => "Name already in use."];      
-                              
-                              
+       
         //Check if user has changed the name                      
         if ($request -> session()->get('userId') != null) {
             if ($userFound -> name == $request -> newName) {
-                $validName = (object) ['changed' => false, 'error' => "Name has not been modified, no changes were made."];  
+                $validName = (object) ['willChange' => false, 'error' => "Name has not been modified, no changes were made."];  
             } else {
                 //If the name has been modified, we check that is valid.
-                if (($request -> newName < 3) || ($request -> newName > 20)) {
+                if (( strlen ($request -> newName) < 3) || ( strlen($request -> newName) > 20)) {
                     $validName = (object) ['willChange' => false, 'error' => "Name must have a minimum amount of 3 characters and 20 max."];
                 } else {
                     //If the name is valid we check if it's not repeated.
@@ -79,6 +84,7 @@ class UserController extends Controller
                             $nameRepeated = true;
                         }
                     }
+                    //Check if the name is repeated.
                     if ($nameRepeated) {
                         $validName = (object) ['willChange' => false,
                         'error' => "Name already in use."]; 
@@ -92,12 +98,17 @@ class UserController extends Controller
         return $validName;
     }
     
+
     public function setUsername(Request $request)
     {
         $validName = (object) ['willChange' => true];
+
+        //Check if the user id is not, if not null we continue to check
         if ($request -> session()->get('userId') != null) {
             $userFound = User::where('id', $request->session()->get('userId'))->first();
             $validName = $this->checkValidName($request, $userFound);
+
+            //If after validating the name can be changed we change it, if not we return the error.
             if ($validName -> willChange) {
                 $userFound -> name = $request -> newName;
                 $userFound -> save(); 
