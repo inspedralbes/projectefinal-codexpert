19ec7eafdc65e311ae6979ee975c8a730bc88985
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-04-25 09:43:30 +0200

    env variable config changed

--- back/codexpert-laravel/app/Http/Middleware/Cors.php
+++ back/codexpert-laravel/app/Http/Middleware/Cors.php
@@ -15,7 +15,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', env('FRONT_ROUTE'))
+        return $next($request)->header('Access-Control-Allow-Origin', config('addresses.front_route'))
             ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
             ->header('Access-Control-Allow-Credentials', "true")
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
