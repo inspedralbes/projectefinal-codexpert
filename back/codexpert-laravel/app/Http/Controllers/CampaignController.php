c00ec118bb7198d25f1b7c88c677ba9d5dfb5fb9
a21alecrinor <a21alecrinor@inspedralbes.cat>
2023-04-28 10:33:35 +0200

    Added campaign questions, inputs and tests

---
+++ back/codexpert-laravel/app/Http/Controllers/CampaignController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Game;
+use App\Models\Question;
+use App\Models\Test_input;
+use App\Models\Test_output;
+use App\Models\Game_question;
+use App\Models\User_game;
+use App\Models\User;
+use App\Models\Campaign_question;
+
+class CampaignController extends Controller
+{
+ 
+     
+}
---
+++ back/codexpert-laravel/database/migrations/2023_04_28_081254_campaign_test_outputs.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaign_test_outputs', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('question_id')->unsigned()->index();
+            $table->string('output') -> nullable();
+            $table->foreign('question_id')->references('id')->on('campaign_questions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaign_test_outputs');
+    }
+};
