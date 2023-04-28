25f741ed1de4f775fd3a1c971bad252249b4b351
marti-sv <a21marsalval@inspedralbes.cat>
2023-04-28 10:30:26 +0200

    ranking front

--- front/src/pages/Ranking.js
+++ front/src/pages/Ranking.js
@@ -2,9 +2,77 @@ import React from 'react'
 import '../styles/normalize.css'
 
 function Ranking() {
+  let usersList = []
   return (
     <div>
-      <h1>RANKING DE PARTIDA</h1>
+      <h1>RANKING</h1>
+      <table>
+        <thead>
+          <th>
+            Username
+          </th>
+          <th>
+            ELO
+          </th>
+          <th>
+            LVL
+          </th>
+        </thead>
+        <tbody>
+          {Array.isArray(usersList) && usersList.map((element, index) => {
+            <tr>
+              <td>
+                {element.avatar}
+                {element.name}
+              </td>
+              <td>
+                {element.elo}
+              </td>
+              <td>
+                {element.xp}
+              </td>
+            </tr>
+          })
+          }
+        </tbody>
+      </table>
+
+      <div className='table__body'>
+        {Array.isArray(usersList)
+          ? usersList.map((element, index) => {
+            return (
+              <li
+                className='table__row row'
+                //onClick={handleJoin}
+                key={index}
+                id={element.lobby_name}
+              >
+                <div
+                  id={element.lobby_name}
+                  className='col col-1'
+                  data-label='Lobby Id'
+                >
+                  {index + 1}
+                </div>
+                <div
+                  id={element.lobby_name}
+                  className='col col-2'
+                  data-label='Lobby Name'
+                >
+                  {element.lobby_name}
+                </div>
+                <div
+                  id={element.lobby_name}
+                  className='col col-3'
+                  data-label='Owner'
+                >
+                  {element.owner_name}
+                </div>
+              </li>
+            )
+          })
+          : null}
+      </div>
     </div>
   )
 }
