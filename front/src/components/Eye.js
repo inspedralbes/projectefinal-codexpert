b8df375b75c740b8eec38e13c1980656a65e4280
a21alecrinor <a21alecrinor@inspedralbes.cat>
2023-05-04 13:22:17 +0200

    fixed eye at password inputs

--- front/src/components/Eye.js
+++ front/src/components/Eye.js
@@ -1,25 +1,31 @@
 import '../styles/normalize.css'
+import '../styles/eye.css'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
+import openEye from '../img/openEye.png'
+import closedEye from '../img/closedEye.png'
 
 Eye.propTypes = {
   id: PropTypes.string
 }
 
 function Eye({ id }) {
+  const [pwdType, setPwdType] = useState(openEye)
   const handleEye = () => {
     const pwd = document.getElementById(id)
     if (pwd.type === 'password') {
       pwd.type = 'text'
+      setPwdType(closedEye)
     } else {
       pwd.type = 'password'
+      setPwdType(openEye)
     }
   }
 
   return (
-    <>
-      <button onClick={() => handleEye()}>Eie</button>
-    </>
+    <div className='eye'>
+      <button className='invisible eyeBtn' onClick={() => handleEye()}><img src={pwdType} width={'30px'} alt=''></img></button>
+    </div>
   )
 }
 
