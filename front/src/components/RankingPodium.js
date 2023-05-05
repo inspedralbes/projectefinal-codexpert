5de9db33953ed1fc6412a1e25dee152931f8ee69
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-05-05 09:38:56 +0200

    ranking y lista separada

--- front/src/components/RankingPodium.js
+++ front/src/components/RankingPodium.js
@@ -71,42 +71,7 @@ function RankingPodium({ rankingData }) {
 
             </div>
           )
-        } else {
-          return (
-            <div key={index}>
-              <table className='ranking__table' >
-                <thead>
-                  <tr>
-                    <th className='ranking__position'>Posición</th>
-                    <th className='ranking__profile'>Perfil</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  {Array.isArray(rankingData) && rankingData.map((element, index) => {
-                    if (index > 2) {
-                      return (
-                        <tr key={index}>
-                          <td className='ranking__position'>{index + 1}</td>
-                          <td className='ranking__profile'>
-                            <img
-                              src={element.avatar}
-                              width='50px'
-                              className='user__image'
-                              alt={element.name + '\'s avatar'} />
-                            <h1>{element.name}</h1>
-                          </td>
-                        </tr>
-                      )
-                    } else {
-                      return null
-                    }
-                  })}
-                </tbody>
-              </table>
-            </div >
-          )
-        }
+        } else return null
       })
       }
     </div >
