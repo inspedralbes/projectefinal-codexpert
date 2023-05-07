40cac280c2c05801b8c9209dfcfbffc601ee9b61
Marti-SV <a21marsalval@inspedralbes.cat>
2023-05-07 17:31:19 +0200

    error styles

--- front/src/pages/404.js
+++ front/src/pages/404.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import '../styles/normalize.css'
+import '../styles/404.css'
 import { Link } from 'react-router-dom'
+import ErrorGif from '../img/Error.gif'
+import { useNavigate } from 'react-router-dom' // Rutas
 
 function E404() {
+  const navigate = useNavigate()
+  if (localStorage.getItem('lastPage') !== null) {
+    navigate('/profile')
+  } else {
+    navigate('/')
+  }
   return (
-    <div>
-      <h1>404 Error</h1>
-      <Link to='/'>
-        <button>Go to Home</button>
-      </Link>
+    <div className='E404'>
+      <img src={ErrorGif}></img>
+      <h1>404 ERROR</h1>
+        <button className='pixel-button' onClick={() => localStorage.getItem("lastPage") !== undefined ? navigate("/" + localStorage.getItem("lastPage")) : navigate('/')}>Go back</button>
     </div>
   )
 }
