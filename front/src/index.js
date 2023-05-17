5bc9b86fdfaba47932059963fbecebab681a545f
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-05-17 12:05:06 +0200

    codi mort

--- front/src/index.js
+++ front/src/index.js
@@ -33,10 +33,6 @@ if (cookies.get('token') !== undefined && cookies.get('token') !== null) {
   }, '*')
 }
 
-// useEffect(() => {
-
-// }, [])
-
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode>
