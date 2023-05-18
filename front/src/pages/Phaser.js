9e3ec6d3b6be9c7401362ddccd0855458e2b4809
GasparGomez <a21gasgomtor@inspedralbes.cat>
2023-05-18 10:26:22 +0200

    TG-323 #closed

--- front/src/pages/Phaser.js
+++ front/src/pages/Phaser.js
@@ -1,9 +1,15 @@
+import * as Phaser from 'phaser'
+
+import Preloader from '../Phaser/scenes/Preloader'
+import Game from '../Phaser/scenes/Game'
+import InteractUI from '../Phaser/scenes/InteractUI'
+
 import '../styles/normalize.css'
-import React, { useEffect } from 'react'
-import '../PhaserGame'
+import React, { useEffect, useRef } from 'react'
+// import '../PhaserGame'
 import { useNavigate } from 'react-router'
 
-function Phaser() {
+const CodeWorld = () => {
   const navigate = useNavigate()
 
   const handleMessage = (event) => {
@@ -18,16 +24,52 @@ function Phaser() {
     }
   }
 
+  const parentRef = useRef(null)
+  let game = null
+
   useEffect(() => {
+    if (parentRef.current) {
+      const config = {
+        // Configuración de Phaser
+        type: Phaser.AUTO,
+        parent: parentRef.current,
+        backgroundColor: '#60A0A8',
+        scale: {
+          // mode: Phaser.Scale.ScaleModes.RESIZE,
+          width: window.innerWidth / 2,
+          height: window.innerHeight / 2,
+          zoom: 2
+        },
+        dom: {
+          createContainer: true
+        },
+        physics: {
+          default: 'arcade',
+          arcade: {
+            gravity: { y: 0 },
+            debug: true
+          }
+        },
+        scene: [Preloader, Game, InteractUI]
+      }
+      game = new Phaser.Game(config)
+    }
+
     window.addEventListener('message', handleMessage)
 
     return () => {
+      if (game) {
+        // Realizar las tareas de limpieza de Phaser si es necesario
+        game.destroy()
+        game = null
+      }
       window.removeEventListener('message', handleMessage)
     }
   }, [])
+
   return (
-    <div id='phaser-container'></div>
+    <div ref={parentRef}></div>
   )
 }
 
-export default Phaser
+export default CodeWorld
